b73a79c81f881dfa080d2265619aa3a39422fc48
Keith Wallace <jkwallac@ualberta.ca>
2023-02-16 14:07:30 -0700

    Removed fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,7 @@
 const searchInput = document.querySelector("#input");
 const pokemon = document.querySelector("#pokemon");
 // Removes focus after pressing enter on mobile keyboard
-key.keyCode === 13
-const hideMobileKeyboardOnReturn = (element) => {
-    element.addEventListener('keyup', (keyboardEvent) => {
-        const key = keyboardEvent.code || keyboardEvent.keyCode;
-        if (key === 'Enter' || key === 13) {
-            element.blur();
-        }
-    });
-};
+
 
 
 //Triggers "Search" button press if "Enter" key is pressed
